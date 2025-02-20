496718bab9bf2c1694ba4edd4515f4bae99aae3d
mongi-grissa <grissamongi@gmail.com>
2025-02-20 17:14:31 +0100

    ex2 et ex3

--- tp7_3.java
+++ tp7_3.java
@@ -1,40 +1,46 @@
 import java.util.Scanner;
 
-public class tp7_2 {
+public class tp7_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nl, nc;
         do {
             System.out.print("Entrez ligne");
             nl = sc.nextInt();
-        } while (nl <= 0 || nl >= 30);
+        } while (nl <= 0 || nl > 20);
         do {
-            System.out.print("Entrez colone ");
+            System.out.print("Entrez colpne ");
             nc = sc.nextInt();
-        } while (nc <= 0 || nc >= 30);
+        } while (nc <= 0 || nc > 30);
         int[][] M = new int[nl][nc];
-        System.out.println("Entrez la matrice :");
+        System.out.println("Entrez les éléments de la matrice :");
         for (int i = 0; i < nl; i++) {
             for (int j = 0; j < nc; j++) {
                 System.out.print("M[" + i + "][" + j + "] = ");
                 M[i][j] = sc.nextInt();
             }
         }
-        int sum = 0;
-        long product = 1;
+        int[] T = new int[nl];
         for (int i = 0; i < nl; i++) {
+            int sum = 0;
             for (int j = 0; j < nc; j++) {
                 sum += M[i][j];
-                product *= M[i][j];
+            }
+            T[i] = sum;
+        }
+        int min = T[0];
+        int max = T[0];
+        for (int i = 1; i < nl; i++) {
+            if (T[i] < min) {
+                min = T[i];
+            }
+            if (T[i] > max) {
+                max = T[i];
             }
         }
-        
-        int totalElements = nl * nc;
-        double average = (double) sum / totalElements;
         System.out.println("\nRésultats :");
-        System.out.println("Somme des éléments : " + sum);
-        System.out.println("Produit des éléments : " + product);
-        System.out.println("Moyenne des éléments : " + average);
+        System.out.println("Somme minimale parmi les lignes : " + min);
+        System.out.println("Somme maximale parmi les lignes : " + max);
         
         sc.close();
     }
